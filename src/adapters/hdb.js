443b552b7df8af4fa16b5210ff701b4407c05624
addmarcelofx <marcelo.xavier@addvisor.com.br>
2023-09-14 18:05:32 -0300

    MFX-updateHDB

--- src/adapters/hdb.js
+++ src/adapters/hdb.js
@@ -44,7 +44,6 @@ class HDBAdapter extends BaseAdapter {
 	 * @param {Service} service
 	 */
 	 init(service) {
-		debugger;
 		super.init(service);
 
 		if (!this.opts.collection) {
@@ -67,7 +66,6 @@ class HDBAdapter extends BaseAdapter {
 	 * Connect adapter to database
 	 */
 	async connect() {
-		debugger;
 		if(!this.connectionObj){
 			try{
 				let connParams = {
@@ -121,7 +119,22 @@ class HDBAdapter extends BaseAdapter {
 	 * @returns {Promise<Array>}
 	 */
 	find(params) {
-		return this.connectionObj.exec('select * from ADDTAX.TAXD0000');
+		debugger;
+		let schema = this.opts?.schema ? this.opts.schema : undefined;
+		let table = this.opts?.collection ? this.opts.collection : undefined;
+		let sqlQuery = 'select * from ' + schema + '.' + table; //Ex: 'select * from ADDTAX.TAXD0000'
+		if(params?.query){
+			let query = this.createQuery(params);
+			if(query)
+				sqlQuery = sqlQuery + ' ' + query;
+		}
+		
+		let response = this.connectionObj.exec(sqlQuery);
+		if(response)
+			response.forEach((elem) => {
+				elem._id = elem._ID;	
+			});
+		return response;
 		// return this.createQuery(params).toArray();
 	}
 
@@ -222,11 +235,38 @@ class HDBAdapter extends BaseAdapter {
 	 *
 	 */
 	async insert(entity) {
-		if (entity._id) {
-			entity._id = this.stringToObjectID(entity._id);
+		debugger;
+		let schema = this.opts?.schema ? this.opts.schema : undefined;
+		let table = this.opts?.collection ? this.opts.collection : undefined;
+		let queryFields = '';
+		let queryValues = '';
+
+		this.service.$fields.forEach((field) => {
+			//if(entity[field.columnName] && field.columnName != "_id"){
+			if(entity[field.columnName]){
+				queryFields = '' + queryFields + field.columnName + ', ';
+				if(field.columnType == 'string')
+					queryValues = '' + queryValues + "'" + entity[field.columnName] + "', ";
+				else
+					queryValues = '' + queryValues + entity[field.columnName] + ', ';
+			}
+		
+		});
+
+		queryFields = queryFields.substring(0, queryFields.length - 2);
+		queryValues = queryValues.substring(0, queryValues.length - 2);
+
+		//let sqlQuery = 'insert into ' + schema + '.' + table + '(' + Object.keys(entity) + ') VALUES(' + Object.values(entity) + ')'; //Ex: 'INSERT INTO ADDTAX.TAXD0000(id, indoc) VALUES(12321231, 1)'
+		let sqlQuery = 'insert into ' + schema + '.' + table + '(' + queryFields + ') VALUES(' + queryValues + ')'; //Ex: 'INSERT INTO ADDTAX.TAXD0000(id, indoc) VALUES(12321231, 1)'
+		let insertResult;
+		try{
+			insertResult = this.connectionObj.exec(sqlQuery); //Ex: [{COUNT(*): 1}]
+		}catch(err){
+			//TODO: Tratar erro de query
 		}
-		const res = await this.collection.insertOne(entity);
-		if (!res.acknowledged) throw new Error("MongoDB insertOne failed.");
+		if(insertResult){
+			return entity;
+		}
 		return entity;
 	}
 
@@ -260,18 +300,49 @@ class HDBAdapter extends BaseAdapter {
 	 *
 	 */
 	async updateById(id, changes, opts) {
-		const raw = opts && opts.raw ? true : false;
-		if (!raw) {
-			// Flatten the changes to dot notation
-			changes = flatten(changes, { safe: true });
+		if(id){
+			let schema = this.opts?.schema ? this.opts.schema : undefined;
+			let table = this.opts?.collection ? this.opts.collection : undefined;
+			let setString = '';
+			Object.entries(changes).forEach(([key, value]) => {
+				let keyDef = this.service.$fields.find((elem) => elem?.columnName == key);
+				let keyIsString = false;
+				if(keyDef && keyDef?.columnType == 'string')
+					keyIsString = true;
+				if(keyIsString)
+					setString = setString + ' ' + key + "='" + value + "',"
+				else
+				setString = setString + ' ' + key + "=" + value + ","
+			});
+			if(setString){
+				setString = setString.substring(0, setString.length - 1);
+				let sqlQuery = "UPDATE " + schema + '.' + table + " SET " + setString + "WHERE _ID = '" + id + "'"; //Ex.: UPDATE ADDTAX.TAXD0000 SET DOMIN = 'SP 3550308', PROCX='03500' WHERE _ID = '26b812d0-51a7-11ee-94c4-e7045b3b40cd' 				
+				let insertResult;
+				try{
+					insertResult = this.connectionObj.exec(sqlQuery); //Ex: [{COUNT(*): 1}]
+				}catch(err){
+					//TODO: Tratar erro de query
+				}
+				if(insertResult){
+					let findResult = await this.find({query: { id:id}});
+					if(findResult)
+						return findResult[0];
+				}
+			};
 		}
+		// const raw = opts && opts.raw ? true : false;
+		// if (!raw) {
+		// 	// Flatten the changes to dot notation
+		// 	changes = flatten(changes, { safe: true });
+		// }
 
-		const res = await this.collection.findOneAndUpdate(
-			{ _id: this.stringToObjectID(id) },
-			raw ? changes : { $set: changes },
-			{ returnDocument: "after" }
-		);
-		return res.value;
+		// const res = await this.collection.findOneAndUpdate(
+		// 	{ _ID: this.stringToObjectID(id) },
+		// 	raw ? changes : { $set: changes },
+		// 	{ returnDocument: "after" }
+		// );
+		// return res.value;
+		return;
 	}
 
 	/**
@@ -377,89 +448,89 @@ class HDBAdapter extends BaseAdapter {
 		return query;
 	}
 
-	/**
-	 * Create a query based on filters
-	 *
-	 * Available filters:
-	 *  - search
-	 *  - searchFields
-	 * 	- sort
-	 * 	- limit
-	 * 	- offset
-	 *  - query
-	 *
-	 * @param {Object} params
-	 * @param {Object?} opts
-	 * @param {Boolean?} opts.counting
-	 * @returns {Query}
-	 * @memberof MemoryDbAdapter
-	 */
-	createQuery(params, opts = {}) {
-		const fn = opts.counting ? this.collection.countDocuments : this.collection.find;
-		let q;
-		if (params) {
-			if (_.isString(params.search) && params.search !== "") {
-				// Full-text search
-				// More info: https://docs.mongodb.com/manual/reference/operator/query/text/
-				q = fn.call(
-					this.collection,
-					Object.assign({}, params.query || {}, {
-						$text: {
-							$search: params.search
-						}
-					})
-				);
+	// /**
+	//  * Create a query based on filters
+	//  *
+	//  * Available filters:
+	//  *  - search
+	//  *  - searchFields
+	//  * 	- sort
+	//  * 	- limit
+	//  * 	- offset
+	//  *  - query
+	//  *
+	//  * @param {Object} params
+	//  * @param {Object?} opts
+	//  * @param {Boolean?} opts.counting
+	//  * @returns {Query}
+	//  * @memberof MemoryDbAdapter
+	//  */
+	// createQuery(params, opts = {}) {
+	// 	const fn = opts.counting ? this.collection.countDocuments : this.collection.find;
+	// 	let q;
+	// 	if (params) {
+	// 		if (_.isString(params.search) && params.search !== "") {
+	// 			// Full-text search
+	// 			// More info: https://docs.mongodb.com/manual/reference/operator/query/text/
+	// 			q = fn.call(
+	// 				this.collection,
+	// 				Object.assign({}, params.query || {}, {
+	// 					$text: {
+	// 						$search: params.search
+	// 					}
+	// 				})
+	// 			);
 
-				if (!opts.counting) {
-					if (q.project) q.project({ _score: { $meta: "textScore" } });
+	// 			if (!opts.counting) {
+	// 				if (q.project) q.project({ _score: { $meta: "textScore" } });
 
-					if (q.sort) {
-						if (params.sort) {
-							const sort = this.transformSort(params.sort);
-							if (sort) q.sort(sort);
-						} else {
-							q.sort({
-								_score: {
-									$meta: "textScore"
-								}
-							});
-						}
-					}
-				}
-			} else {
-				const query = this.convertIDToObjectID(params.query);
+	// 				if (q.sort) {
+	// 					if (params.sort) {
+	// 						const sort = this.transformSort(params.sort);
+	// 						if (sort) q.sort(sort);
+	// 					} else {
+	// 						q.sort({
+	// 							_score: {
+	// 								$meta: "textScore"
+	// 							}
+	// 						});
+	// 					}
+	// 				}
+	// 			}
+	// 		} else {
+	// 			const query = this.convertIDToObjectID(params.query);
 
-				q = fn.call(this.collection, query);
+	// 			q = fn.call(this.collection, query);
 
-				// Sort
-				if (!opts.counting && params.sort && q.sort) {
-					const sort = this.transformSort(params.sort);
-					if (sort) q.sort(sort);
+	// 			// Sort
+	// 			if (!opts.counting && params.sort && q.sort) {
+	// 				const sort = this.transformSort(params.sort);
+	// 				if (sort) q.sort(sort);
 
-					// Collation
-					// https://docs.mongodb.com/manual/reference/method/cursor.collation/
-					if (params.collation) q.collation(params.collation);
-				}
-			}
+	// 				// Collation
+	// 				// https://docs.mongodb.com/manual/reference/method/cursor.collation/
+	// 				if (params.collation) q.collation(params.collation);
+	// 			}
+	// 		}
 
-			if (!opts.counting) {
-				// Offset
-				if (_.isNumber(params.offset) && params.offset > 0) q.skip(params.offset);
+	// 		if (!opts.counting) {
+	// 			// Offset
+	// 			if (_.isNumber(params.offset) && params.offset > 0) q.skip(params.offset);
 
-				// Limit
-				if (_.isNumber(params.limit) && params.limit > 0) q.limit(params.limit);
-			}
+	// 			// Limit
+	// 			if (_.isNumber(params.limit) && params.limit > 0) q.limit(params.limit);
+	// 		}
 
-			// Hint
-			// https://docs.mongodb.com/manual/reference/method/cursor.hint/
-			if (params.hint) q.hint(params.hint);
+	// 		// Hint
+	// 		// https://docs.mongodb.com/manual/reference/method/cursor.hint/
+	// 		if (params.hint) q.hint(params.hint);
 
-			return q;
-		}
+	// 		return q;
+	// 	}
 
-		// If not params
-		return fn.call(this.collection, {});
-	}
+	// 	// If not params
+	// 	return fn.call(this.collection, {});
+	// }
 
 	/**
 	 * Convert the `sort` param to a `sort` object to Mongo queries.
@@ -516,6 +587,149 @@ class HDBAdapter extends BaseAdapter {
 	removeIndex(def) {
 		if (def.name) return this.collection.dropIndex(def.name);
 		else return this.collection.dropIndex(def.fields);
+	}
+
+	/**
+	 * Create a query based on filters
+	 *
+	 * Available filters:
+	 *  - search
+	 *  - searchFields
+	 * 	- sort
+	 * 	- limit
+	 * 	- offset
+	 *  - query
+	 *
+	 * @param {Object} params
+	 * @param {Object?} opts
+	 * @param {Boolean?} opts.counting
+	 * @returns {Query}
+	 * @memberof MemoryDbAdapter
+	 */
+	createQuery(params, opts = {}) {
+		//let q = this.getTable();
+		let q = 'WHERE ';
+		//if (opts.counting) q = q.count({ count: "*" });
+		if (params) {
+			const query = params.query ? Object.assign({}, params.query) : {};
+
+			Object.entries(query).forEach(([key, value]) => {
+				let keyDef = this.service.$fields.find((elem) => elem?.columnName == key);
+				let keyIsString = false;
+				if(key == 'id' || key == '_id'){
+					key = '_ID';
+					keyIsString = true;
+				}
+				if(keyDef && keyDef?.columnType == 'string')
+					keyIsString = true;
+				if (typeof value == "object" && value != null) {
+					if (value.$in && Array.isArray(value.$in)) {
+						//q = q.whereIn(key, value.$in);
+						q = q + 'IMPLEMENTAR';
+					} else if (value.$nin && Array.isArray(value.$nin)) {
+						//q = q.whereNotIn(key, value.$nin);
+						q = q + 'IMPLEMENTAR';
+					} else if (value.$gt) {
+						//q = q.where(key, ">", value.$gt);
+						if(keyIsString)
+							q = q + key + " > '" + value.$gt + "' AND ";
+						else
+							q = q + key + ' > ' + value.$gt + ' AND ';
+					} else if (value.$gte) {
+						//q = q.where(key, ">=", value.$gte);
+						if(keyIsString)
+							q = q + key + " >= '" + value.$gte + "' AND ";
+						else
+							q = q + key + ' >= ' + value.$gte + ' AND ';
+					} else if (value.$lt) {
+						//q = q.where(key, "<", value.$lt);
+						if(keyIsString)
+							q = q + key + " < '" + value.$lt + "' AND ";
+						else
+							q = q + key + ' < ' + value.$lt + ' AND ';
+					} else if (value.$lte) {
+						//q = q.where(key, "<=", value.$lte);
+						if(keyIsString)
+							q = q + key + " <= '" + value.$lte + "' AND ";
+						else
+							q = q + key + ' <= ' + value.$lte + ' AND ';
+					} else if (value.$eq) {
+						//q = q.where(key, "=", value.$eq);
+						if(keyIsString)
+							q = q + key + " = '" + value.$eq + "' AND ";
+						else
+							q = q + key + ' = ' + value.$eq + ' AND ';
+					} else if (value.$ne) {
+						//q = q.where(key, "=", value.$ne);
+						if(keyIsString)
+							q = q + key + " != '" + value.$ne + "' AND ";
+						else
+							q = q + key + ' != ' + value.$ne + ' AND ';
+					} else if (value.$exists === true) {
+						//q = q.whereNotNull(key);
+						q = q + 'IMPLEMENTAR';
+					} else if (value.$exists === false) {
+						//q = q.whereNull(key);
+						q = q + 'IMPLEMENTAR';
+					} else if (value.$raw) {
+						if (typeof value.$raw == "string") {
+							//q = q.whereRaw(value.$raw);
+							q = q + 'IMPLEMENTAR';
+						} else if (typeof value.$raw == "object") {
+							//q = q.whereRaw(value.$raw.condition, value.$raw.bindings);
+							q = q + 'IMPLEMENTAR';
+						}
+					}
+				} else {
+					//q = q.where(key, value);
+					if(keyIsString)
+						q = q + key + " = '" + value + "' AND ";
+					else
+						q = q + key + ' = ' + value + ' AND ';
+				}
+			});
+
+			//TODO: IMPLEMENTAR FUNCIONALIDADES ABAIXO
+			// Text search
+			// if (_.isString(params.search) && params.search !== "" && params.searchFields) {
+			// 	params.searchFields.forEach((field, i) => {
+			// 		const fn = i == 0 ? "where" : "orWhere";
+			// 		q = q[fn](field, "like", `%${params.search}%`);
+			// 	});
+			// }
+
+			// // Sort
+			// if (!opts.counting && params.sort) {
+			// 	let pSort = params.sort;
+			// 	if (typeof pSort == "string") pSort = [pSort];
+			// 	pSort.forEach(field => {
+			// 		if (field.startsWith("-")) q = q.orderBy(field.slice(1), "desc");
+			// 		else q = q.orderBy(field, "asc");
+			// 	});
+			// }
+
+			// // Limit
+			// if (!opts.counting && _.isNumber(params.limit) && params.limit > 0)
+			// 	q.limit(params.limit);
+
+			// // Offset
+			// if (!opts.counting && _.isNumber(params.offset) && params.offset > 0) {
+			// 	if (!params.sort && this.opts.knex.client == "mssql") {
+			// 		// MSSQL can't use offset without sort.
+			// 		// https://github.com/knex/knex/issues/1527
+			// 		q = q.orderBy(this.idFieldName, "asc");
+			// 	}
+			// 	q.offset(params.offset);
+			// }
+		}
+
+		// If not params
+		if(q != 'WHERE '){
+			q = q.substring(0, q.length - 5); //Remove o ' AND ' do final da string
+			return q;
+		}
+		else
+			return '';
 	}
 }
 
